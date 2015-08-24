6d7abc3adb153e67e03cdc7d39fa41b416a753b9
Rajan Paneru <paneru.rajan@gmail.com>
2015-08-24 09:07:11 +0545

    inserted

---
+++ index.php
@@ -0,0 +1,22 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+</head>
+<body>
+<div class="well well-sm">
+    <ul id="nav">
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+    </ul>
+
+</div>
+
+<script type="text/javascript">
+
+</script>
+</body>
+</html>
